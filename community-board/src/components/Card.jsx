fe9cda8ccabe485fb4b092e6697748ac91be2f05
Epifanio Sarinana <sarinanaepifanio@gmail.com>
2024-09-25 21:19:48 -0600

    Added a css for card component and added 2 card items

--- community-board/src/components/Card.jsx
+++ community-board/src/components/Card.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import './Card.css';
 
 const Card = () => {
     return (
         <div className="card-container">
-            <div className="card-card"></div>
-            <div className="card-card"></div>
-            <div className="card-card"></div>
-            <div className="card-card"></div>
-            <div className="card-card"></div>
+            <div className="card-card">
+                <h3 className="card-card-title">Algorithms</h3>
+                <h4 className="card-card-description">These will show you the most popular Algorithms and how they work and are used</h4>
+            </div>
+            <div className="card-card">
+                <h3 className="card-card-title">CyberSecurity</h3>
+                <h4 className="card-card-description">This page will give you more information on what is CyberSecurity and how it is important.</h4>
+            </div>
+            <div className="card-card">
+                <h3 className="card-card-title"></h3>
+                <h4 className="card-card-description"></h4>
+            </div>
+            <div className="card-card">
+                <h3 className="card-card-title"></h3>
+                <h4 className="card-card-description"></h4>
+            </div>
         </div>
     )
 }
