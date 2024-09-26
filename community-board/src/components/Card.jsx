18185ae31137f1bb0b717bcb09c588b715527f18
Epifanio Sarinana <sarinanaepifanio@gmail.com>
2024-09-25 21:36:02 -0600

    Added css to space all my cards and added more card right along with a title and description

--- community-board/src/components/Card.jsx
+++ community-board/src/components/Card.jsx
@@ -6,19 +6,23 @@ const Card = () => {
         <div className="card-container">
             <div className="card-card">
                 <h3 className="card-card-title">Algorithms</h3>
-                <h4 className="card-card-description">These will show you the most popular Algorithms and how they work and are used</h4>
+                <h4 className="card-card-description">These will show you the most popular Algorithms and how they work and are used.</h4>
+                <button>Click Me!</button>
             </div>
             <div className="card-card">
                 <h3 className="card-card-title">CyberSecurity</h3>
                 <h4 className="card-card-description">This page will give you more information on what is CyberSecurity and how it is important.</h4>
+                <button>Click Me!</button>
             </div>
             <div className="card-card">
-                <h3 className="card-card-title"></h3>
-                <h4 className="card-card-description"></h4>
+                <h3 className="card-card-title">Computer Science</h3>
+                <h4 className="card-card-description">This page will provide you with an overview of what computer science is and all fields it covers.</h4>
+                <button>Click Me!</button>
             </div>
             <div className="card-card">
-                <h3 className="card-card-title"></h3>
-                <h4 className="card-card-description"></h4>
+                <h3 className="card-card-title">Computer Architecture</h3>
+                <h4 className="card-card-description">This page will provide you with more information on the Architecture of computers and compilers.</h4>
+                <button>Click Me!</button>
             </div>
         </div>
     )
